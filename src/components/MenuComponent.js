2271c684b2b463a6838c6174773cc26d0c06af3e
abhishek_gupta <abhishek_gupta@seologistics.com>
2018-08-17 10:04:06 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,7 +7,7 @@ import {
 function RenderMenuItem({ dish, onClick }) {
     return (
         <Card
-            onClick={() => onClick(dish.id)}>
+            /* onClick={() => onClick(dish.id)} */>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
