f3f6656266a5dc656e5fc7d618836e2e1c3d92bd
abhishek_gupta <abhishek_gupta@seologistics.com>
2018-08-23 09:35:30 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,12 +4,14 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import DishDetail from "./DishdetailComponent";
+
 
 class Main extends Component {
 
@@ -43,7 +45,7 @@ class Main extends Component {
                     comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             );
         };
-
+       
         return (
             <div>
                 <Header />
@@ -52,6 +54,7 @@ class Main extends Component {
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
