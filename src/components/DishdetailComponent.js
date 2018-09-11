b81bae501df76bdc9f813624988961ad2a0763d7
abhishek_gupta <abhishek_gupta@seologistics.com>
2018-09-11 10:23:42 +0530

    Fetch Handling Errors

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,6 +19,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import {Loading} from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
 
 import { Control, LocalForm, Errors } from "react-redux-form";
 const required = val => val && val.length;
@@ -131,7 +132,7 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
   return (
     <Card>
-      <CardImg top src={dish.image} alt={dish.name} />
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
       <CardBody>
         <CardTitle>{dish.name}</CardTitle>
         <CardText>{dish.description}</CardText>
